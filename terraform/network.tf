da8b5ef1dfa4af5354ef16fab0530516eac36f0f
MilanDas <milan.das77@gmail.com>
2020-12-20 18:58:43 -0500

    add all

--- terraform/network.tf
+++ terraform/network.tf
@@ -48,6 +48,16 @@ resource "google_dns_record_set" "dns_grafana" {
   rrdatas = [google_compute_address.external_ip_ingress.address]
 }
 
+resource "google_dns_record_set" "dns_tekton" {
+  name = "tekton.${data.google_dns_managed_zone.env_dns_zone.dns_name}"
+  type = "A"
+  ttl  = 300
+
+  managed_zone = data.google_dns_managed_zone.env_dns_zone.name
+
+  rrdatas = [google_compute_address.external_ip_ingress.address]
+}
+
 
 # grafana.dmilangcp.xyz
 output external_ip_ingress {
